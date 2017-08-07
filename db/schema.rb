60b6d8a653f7bf180eef8ee99b208d344be473b6
Joshua Flack <joshu.alexander@gmail.com>
2017-08-07 13:53:40 -0400

    added default value to user admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807142912) do
+ActiveRecord::Schema.define(version: 20170807175141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 20170807142912) do
     t.string "username"
     t.string "password_digest"
     t.integer "total_score"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
