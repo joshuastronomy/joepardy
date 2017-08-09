4499c98b81e34c88f3e9957677d2d5852a5efd13
Alex <alex@ascholtes.me>
2017-08-09 10:08:47 -0400

    added admin signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,9 @@ class UsersController < ApplicationController
       end
     end
 
-    def admin_toggle
-      @user.toggle!(:admin)
-    end
+    # def admin_toggle
+    #   @user.toggle!(:admin)
+    # end
 
 private
 
